81e9c7ca7541cc524a6a32b8ea7415104f1b314d
Antonio <tonito@umich.edu>
2022-02-26 08:39:05 -0500

    Gallery

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -10,8 +10,9 @@ function Gallery({ currentCategory }) {
         <section>
             <h1 data-testid="h1tag">{capitalizeFirstLetter(name)}</h1>
             <p>{description}</p>
-            <PhotoList />
+            <PhotoList category={currentCategory.name}/>
         </section>
     );
 }
+
 export default Gallery;
